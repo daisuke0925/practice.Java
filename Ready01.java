affd0b534972b015ac4e36ae5737ac730bb3df4e
daisuke0925 <93876241+daisuke0925@users.noreply.github.com>
2022-04-29 19:49:21 +0900

    変数の再宣言

--- Ready01.java
+++ Ready01.java
@@ -1,8 +1,9 @@
 public class Ready {
   public static void main(String[] args) {
-    int age;
-    age = 30;
-    System.out.println(age);
+    int age = 20;
+    System.out.println("私の年齢は" + age);
+    age = 31;
+    System.out.println("…いや、本当の年齢は" + age);
   }
 
 }
