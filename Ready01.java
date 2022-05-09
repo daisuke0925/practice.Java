915924e527f85544e94a9f73672e364cc45e254b
daisuke0925 <93876241+daisuke0925@users.noreply.github.com>
2022-05-09 11:51:01 +0900

    エスケープシーケンを用いていない例

--- Ready01.java
+++ Ready01.java
@@ -1,11 +1,5 @@
 public class Ready {
   public static void main(String[] args) {
-    int a;
-    int b;
-    a = 20;
-    b = a + 5;
-    System.out.println(a);
-    System.out.println(b);
+    System.out.println("私の好きな記号は二重引用符（"）です");
   }
 }
-
